fad9ec393ddfdccc6003d9491bf88d2e6fec95f4
Sablu Hasan <150502592+sablu1234@users.noreply.github.com>
2026-06-24 16:08:48 +0600

    Add plugin author metadata

---
+++ woo-cart.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Woo Cart
+ * Description: A WooCommerce cart plugin.
+ * Version: 1.0.0
+ * Author: sablu hasan
+ * Author URI: https://sablu-hasan.vercel.app/
+ * Text Domain: woo-cart
+ */
